acf7f277311aade507dbb40d23d9a595c6377bb8
정준영 <jeongjy@mrblue.com>
2024-09-12 17:37:21 +0900

    feat: Redis 설정 및 적용

--- src/main/java/hello/redis/BoardService.java
+++ src/main/java/hello/redis/BoardService.java
@@ -1,5 +1,6 @@
 package hello.redis;
 
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -15,6 +16,8 @@ public class BoardService {
         this.boardRepository = boardRepository;
     }
 
+    // 캐시 설정 추가
+    @Cacheable(cacheNames = "getBoards", key = "'boards:page:' + #page +':size' + #size", cacheManager = "boardCacheManager")
     public List<Board> getBoards(int page, int size) {
         Pageable pageable = PageRequest.of(page - 1, size);
         Page<Board> pageOfBoards = boardRepository.findAllByOrderByCreatedAtDesc(pageable);
